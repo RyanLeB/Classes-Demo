363c47b9a1b6467f511234f5f8315806db47bcba
Ryan LeBlanc <ryanleb2003@gmail.com>
2024-01-12 11:41:25 -0400

    Classes made

---
+++ Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Player
+    {
+        // defaults to private
+        
+        // members - fields (variables)
+        public int health;
+        public int shield;
+        public int score;
+        public int x; // position
+        public int y; // position
+        
+        // members - methods
+
+        // constructor
+        public Player()
+        {
+            Console.WriteLine("Player constructed...");
+        
+            
+            // you can initialize in here as well
+            // health = 100;
+            // shield = 0;
+            // score = 0;
+            // x = 0;
+            // y = 0;
+
+
+        }
+        
+        
+        public void Heal()
+        {
+            health = 100;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,13 +8,62 @@ namespace Classes
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // <--- entry point to a program... doesn't matter what class it's in 
         {
             Console.WriteLine("This is a demo about classes in C#");
             Console.WriteLine();
             Console.WriteLine();
 
+            int score; // declaration
+            score = 0;
+            Console.WriteLine("Score: " + score);
 
+            // example array 
+            int[] ammo; // declaration
+            ammo = new int[5]; //instantiation
+            ammo[0] = 5;
+
+            // Player Instance
+            Player player; // declaration
+            player = new Player(); // instantiation
+
+            player.x = 0;
+            player.y = 0;
+            player.health = 50;
+            
+            
+            // Enemy Instances
+            Enemy smallEnemy;
+            smallEnemy = new Enemy();
+
+            Enemy bossEnemy;
+            bossEnemy = new Enemy();
+            
+            // Enemy array
+            Enemy[] smallEnemies;
+            smallEnemies = new Enemy[100];
+            for (int i = 0; i < smallEnemies.Length; i++)
+            {
+                // ... 
+            }
+            
+            smallEnemy.health = 25;
+            smallEnemy.x = 10;
+            smallEnemy.y = 10;
+
+
+            bossEnemy.health = 200;
+
+            Console.WriteLine("Player Health: " +  player.health);
+            Console.WriteLine("Enemy Health: " +  smallEnemy.health);
+            player.Heal();
+            Console.WriteLine("Player Health: " +  player.health);
+            Console.WriteLine();   
+            Console.WriteLine("Boss Health: " +  bossEnemy.health);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey(true);
         }
     }
